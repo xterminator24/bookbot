472de8d76632139e2876968866cc4b24846e3d88
xterminator24 <scott.mckillop@outlook.com>
2024-10-26 12:51:28 -0300

    refactor function names

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
-  print(f"words: {count_words(text)}")
-  print(f"chars: {count_characters(text)}")
+  word_count = get_word_count(text)
+  chars_dict = get_character_dict(text)
+  print(f"words: {word_count}")
+  print(f"chars: {chars_dict}")
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
 
-def count_words(text):
+def get_word_count(text):
   return len(text.split())
 
-def count_characters(text):
+def get_character_dict(text):
   chars = {}
   for char in text:
     if char in chars:
